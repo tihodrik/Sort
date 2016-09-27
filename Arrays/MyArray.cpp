0bf54c9b43444d12da41995099f88712f950e7b6
Darya Tikhomirova <tihodrik@gmail.com>
2016-09-27 22:47:03 +0300

    basic sorts fixed

--- Arrays/MyArray.cpp
+++ Arrays/MyArray.cpp
@@ -39,7 +39,7 @@ int& MyArray::operator[](int index) {
 void MyArray::BubbleSort() {
 	for (int i = 0; i < length - 1; i++) {
 		for (int j = 0; j < length - i - 1; j++) {
-			if (a[j] < a[j + 1]){
+			if (a[j] > a[j + 1]){
 				int tmp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = tmp;
@@ -57,11 +57,14 @@ void MyArray::InsertionSort() {
 			
 			// Найти место для вставки, попутно сдвигая все элементы на 1 вправо
 			int j = i - 1;
-			while (j != 0 && a[j] > tmp)
+			while (j >= 0 && a[j] > tmp) {
 				a[j + 1] = a[j];
+				j--;
+			}
 
-			// Вставить текущий элемент
-			a[j] = tmp;
+			// Вставить текущий элемент: так как a[j] меньше tmp,
+			// то вставлять нужно на (j+1)-е место
+			a[j + 1] = tmp;
 		}
 	}
 }
@@ -70,11 +73,12 @@ void MyArray::SelectionSort() {
 	int minIndex;
 
 	for (int i = 0; i < length - 1; i++) {
-		minIndex = a[i];
+		minIndex = i;
 
-		for (int j = i + 1; j < length; j++)
+		for (int j = i + 1; j < length; j++) {
 			if (a[j] < a[minIndex])
 				minIndex = j;
+		}
 
 		if (minIndex != i) {
 			int tmp = a[minIndex];
