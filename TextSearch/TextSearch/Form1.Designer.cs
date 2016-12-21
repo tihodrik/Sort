576975edcea2dd10afe00d3459f456fb644471b5
Darya Tikhomirova <tihodrik@gmail.com>
2016-12-21 10:40:23 +0300

    TextSearch (KMP algorithm) added

---
+++ TextSearch/TextSearch/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TextSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text = new System.Windows.Forms.TextBox();
+            this.str = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.Result_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(25, 49);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(359, 99);
+            this.text.TabIndex = 0;
+            // 
+            // str
+            // 
+            this.str.Location = new System.Drawing.Point(25, 193);
+            this.str.Name = "str";
+            this.str.Size = new System.Drawing.Size(273, 20);
+            this.str.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(22, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search string";
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(304, 190);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(80, 25);
+            this.button.TabIndex = 7;
+            this.button.Text = "Search";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Result
+            // 
+            this.Result.Enabled = false;
+            this.Result.Location = new System.Drawing.Point(25, 271);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(359, 20);
+            this.Result.TabIndex = 8;
+            // 
+            // Result_label
+            // 
+            this.Result_label.AutoSize = true;
+            this.Result_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result_label.Location = new System.Drawing.Point(22, 251);
+            this.Result_label.Name = "Result_label";
+            this.Result_label.Size = new System.Drawing.Size(48, 17);
+            this.Result_label.TabIndex = 9;
+            this.Result_label.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 333);
+            this.Controls.Add(this.Result_label);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.str);
+            this.Controls.Add(this.text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.TextBox str;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Label Result_label;
+    }
+}
+
---
+++ TextSearch/TextSearch/KMP.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSearch
+{
+    class KMP : SearchAlgorithm
+    {
+        public String Text { get; set; }
+        public String Str { get; set; }
+
+        public KMP(String text, String str)
+        {
+            Text = text;
+            Str = str;
+        }
+
+        // Префикс - позиция, с элементом на которой совпадает текущий элемент
+        public int[] GetPrefix(string str)
+        {
+            int[] res = new int[str.Length];
+            res[0] = 0;
+            int pos = 0;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                while (pos >= 0 && str[pos] != str[i])
+                {
+                    pos--;
+                }
+                pos++;
+                res[i] = pos;
+            }
+            return res;
+        }
+
+        public StringBuilder Search()
+        {
+            int res = -1;
+            if (Text.Length == 0 || Str.Length > Text.Length)
+            {
+                return new StringBuilder().AppendFormat("Position: {0}, compared: {1} times", res, 0);
+            }
+
+            int[] d = GetPrefix(Str);
+            int counter = 1;
+
+            // Search
+            int i = 0, j = 0;
+            while (i < Text.Length && j < Str.Length)
+            {
+                while (j == 0 && Text[i] != Str[j])
+                {
+                    counter++;
+                    i++;
+                }
+                while (j > 0 && Text[i] != Str[j])
+                {
+                    counter++;
+                    i = i - j + d[j-1] + 1;
+                    j = 0;
+                    if (Text.Length - i < Str.Length)
+                    {
+                        return new StringBuilder().AppendFormat("Position: {0}, copmared: {1} times", -1, counter); ;
+                    }
+                }
+                i++;
+                j++;
+
+                if (j == Str.Length)
+                    res = i - j;
+                else
+                    res = -1;
+            }
+             return new StringBuilder().AppendFormat("Position: {0}, copmared: {1} times", res, counter); ;
+        }
+    }
+}
+
